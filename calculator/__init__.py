00a51654eebb4bfe4d15083a33f1e3a2c26cda56
dhb-9 <dhb9@njit.edu>
2024-06-11 15:20:17 -0400

    added all files for part 3

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,23 +1,32 @@
+from decimal import Decimal
+from typing import Callable
 from calculator.calculation import Calculation
+from calculator.calculations import Calculations
 from calculator.operations import add, subtract, multiply, divide
 
 class Calculator:
     @staticmethod
-    def add(a, b):
+    def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        calculation = Calculation(a, b, operation)
+        Calculations.add_calculation(calculation)
+        return calculation.perform()
+
+    @staticmethod
+    def add(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a, b, add)
         return calculation.get_result
     
     @staticmethod
-    def subtract(a, b):
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a, b, subtract)
         return calculation.get_result
     
     @staticmethod
-    def mutliply(a, b):
+    def mutliply(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a, b, multiply)
         return calculation.get_result
     
     @staticmethod
-    def divide(a, b):
+    def divide(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a, b, divide)
         return calculation.get_result
